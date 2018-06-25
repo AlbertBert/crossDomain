dbc7f5a2ee1d25c158bf5edef1b29c444f9ecefb
ruanwen <ruanwen@panda.tv>
2018-06-25 17:01:17 +0800

    readme

---
+++ JSONP/response.js
@@ -0,0 +1,31 @@
+var http = require('http');
+
+// 从url中获取callback和相应的参数
+function fetchQuery(url) {
+  var callback;
+  var resData = {};
+  var query = url.split('?')[1];
+  var argsArr = query.split('&');
+  for (var i=0 ;i< argsArr.length; i++) {
+    var arr = argsArr[i].split('=');
+    if (arr[0] != 'callback') {
+      resData[arr[0]] = arr[1];
+    } else {
+      callback = arr[1];
+    }
+  }
+  return {
+    callback,
+    resData
+  };
+}
+
+var server = http.createServer(function(req, res) {
+  var {callback, resData} = fetchQuery(req.url);
+  console.log(callback, resData);
+  res.writeHead(200, {'Content-type': 'text/plain; charset=utf-8'});
+  //服务端返回值必须是callback(obj)的形式
+  res.end(callback + '(' + JSON.stringify(resData) + ')');
+})
+
+server.listen(3003);
